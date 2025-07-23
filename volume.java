d3962f230ebaeb538a220a0098ec7cf467220ec0
Jaivardhan30328 <2420090104@klh.edu.in>
2025-07-23 11:26:08 +0530

    Add files via upload

---
+++ volume.java
@@ -0,0 +1,15 @@
+package oops;
+
+import java.util.*;
+
+public class volume {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter num:\t");
+		int a=sc.nextInt();
+		double c=((4/3)*(3.14)*(a*a));
+		System.out.println("Volume is:"+c);
+	}
+
+}
+	
